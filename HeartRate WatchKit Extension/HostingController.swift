d98a34e95c1baadb9e8e5a6dcb9a98b6e855b8b8
slavabily <slavabily@me.com>
2020-08-03 15:53:44 +0300

    Initial Commit

---
+++ HeartRate WatchKit Extension/HostingController.swift
@@ -0,0 +1,16 @@
+//
+//  HostingController.swift
+//  HeartRate WatchKit Extension
+//
+//  Created by slava bily on 3/8/20.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
---
+++ HeartRate WatchKit Extension/NotificationView.swift
@@ -0,0 +1,20 @@
+//
+//  NotificationView.swift
+//  HeartRate WatchKit Extension
+//
+//  Created by slava bily on 3/8/20.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
